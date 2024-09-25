b8acb0daa50df3529a25c7d09e72014ec8ddb1cc
AKSHATVerma659 <akshat.verma.cs27@iilm.edu>
2024-09-25 22:59:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDWwlZ_HHVUslVWIX4U6PkxqnThrIdviN4",
-    authDomain: "login-page-7438f.firebaseapp.com",
-    databaseURL: "https://login-page-7438f-default-rtdb.firebaseio.com",
-    projectId: "login-page-7438f",
-    storageBucket: "login-page-7438f.appspot.com",
-    messagingSenderId: "163120352370",
-    appId: "1:163120352370:web:c05cde78c0ae5bc8f406eb"
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWwlZ_HHVUslVWIX4U6PkxqnThrIdviN4",
+  authDomain: "login-page-7438f.firebaseapp.com",
+  databaseURL: "https://login-page-7438f-default-rtdb.firebaseio.com",
+  projectId: "login-page-7438f",
+  storageBucket: "login-page-7438f.appspot.com",
+  messagingSenderId: "163120352370",
+  appId: "1:163120352370:web:c05cde78c0ae5bc8f406eb"
 };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
